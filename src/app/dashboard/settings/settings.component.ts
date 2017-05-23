833d881246c5ecdb000e8a520a5d16e33874af9b
Philipp Joseph <philipp.joseph@fh-duesseldorf.de>
2017-05-23 13:33:36 +0200

    Changed time settings to dropdown.

--- src/app/dashboard/settings/settings.component.ts
+++ src/app/dashboard/settings/settings.component.ts
@@ -12,14 +12,19 @@ export class SettingsComponent implements OnInit {
   loaded = false;
 
   settingKeys = [
-    "OutletDelay",
-    "TimeBoundary"
+    "Config_OutletDelay",
+    "Config_TimeBoundaryHour",
+    "Config_TimeBoundaryMinute"
   ];
 
   userInput:any = {};
-  systemInput:any = {}
+  systemInput:any = {};
 
   currentDelay = 0;
+
+  hours = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23'];
+  minutes = ['00', '15', '30', '45'];
+
   constructor(private restService:RestService) { }
 
   ngOnInit() {  
